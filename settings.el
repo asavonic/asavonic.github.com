b111e91cd0f0f7ae69ac85bc145f7d91e67c7129
Andrew Savonichev <andrew.savonichev@gmail.com>
2017-07-12 01:16:03 +0300

    Copy theme from org-page 'mdo' theme

--- settings.el
+++ settings.el
@@ -3,4 +3,6 @@
   (setq op/site-main-title "Andrew's personal blog")
   (setq op/site-sub-title  "about Programming, Emacs, language study and more")
   (setq op/personal-github-link "https://github.com/asavonic")
+  (setq op/template-directory
+        (concat op/repository-directory "/themes"))
   (setq op/theme 'mdo))
