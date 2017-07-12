b2f343484c552d17f1f97c206cef762f1757f8e4
Andrew Savonichev <andrew.savonichev@gmail.com>
2017-07-12 08:03:38 +0300

    Change settings for html source code highlighing

--- settings.el
+++ settings.el
@@ -3,6 +3,8 @@
   (setq op/site-main-title "Andrew's personal blog")
   (setq op/site-sub-title  "about Programming, Emacs, language study and more")
   (setq op/personal-github-link "https://github.com/asavonic")
-  (setq op/template-directory
+  (setq op/theme-root-directory
         (concat op/repository-directory "/themes"))
-  (setq op/theme 'mdo))
+  (setq op/theme 'asavonic)
+  (setq op/highlight-render 'htmlize)
+  (setq org-html-htmlize-output-type 'css))
