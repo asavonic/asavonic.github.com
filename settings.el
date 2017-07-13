b4aee957c1458d00b99d3225cf96b12910664660
Andrew Savonichev <andrew.savonichev@intel.com>
2017-07-13 10:24:54 +0300

    Add domain name

--- settings.el
+++ settings.el
@@ -1,5 +1,6 @@
 (defun asavonic-blog ()
   (interactive)
+  (setq op/site-domain "asavonic.github.io")
   (setq op/site-main-title "Andrew's personal blog")
   (setq op/site-sub-title  "about Programming, Emacs, language study and more")
   (setq op/personal-github-link "https://github.com/asavonic")
