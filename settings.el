92995ce5bcba0ca23ac3be64abd8dfbafb1ff60f
Andrew Savonichev <andrew.savonichev@intel.com>
2017-07-13 10:25:04 +0300

    Add default categories

--- settings.el
+++ settings.el
@@ -8,4 +8,28 @@
         (concat op/repository-directory "/themes"))
   (setq op/theme 'asavonic)
   (setq op/highlight-render 'htmlize)
-  (setq org-html-htmlize-output-type 'css))
+  (setq org-html-htmlize-output-type 'css)
+
+  (setq op/category-config-alist
+        '(("blog" ;; this is the default configuration
+           :show-meta t
+           :show-comment t
+           :uri-generator op/generate-uri
+           :uri-template "/blog/%y/%m/%d/%t/"
+           :sort-by :date     ;; how to sort the posts
+           :category-index t) ;; generate category index or not
+          ("index"
+           :show-meta nil
+           :show-comment nil
+           :uri-generator op/generate-uri
+           :uri-template "/"
+           :sort-by :date
+           :category-index nil)
+          ("about"
+           :show-meta nil
+           :show-comment nil
+           :uri-generator op/generate-uri
+           :uri-template "/about/"
+           :sort-by :date
+           :category-index nil))))
+
